b7e0b8bd679c74d4811fad116deaeeb9465d7428
zafar ali <zafaralyjamali722@gmail.com>
2024-02-26 13:39:15 +0500

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Zafar Ali on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
